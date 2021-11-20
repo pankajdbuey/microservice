e35ba452ff6079e125d5e3841c4fbf14bbd9d2e4
pankajdbuey <67091771+pankajdbuey@users.noreply.github.com>
2021-11-20 16:21:48 +0530

    Update db_handler.go

--- micro/db/db_handler.go
+++ micro/db/db_handler.go
@@ -30,7 +30,7 @@ func init() {
 	//var cancel context.CancelFunc
 	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 	// defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://db_server:27017"))
 	// defer func() {
 	// 	if err = client.Disconnect(ctx); err != nil {
 	// 		panic(err)
